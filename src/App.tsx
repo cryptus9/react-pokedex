876687c6eb3bdb35259ddf94dfd89755c79d3a82
Jannes <jmuense@it-economics.de>
2022-11-07 11:08:21 +0100

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import "./styles.css";
-import Button from "@mui/material/Button";
-import { useState } from "react";
-import PokeCard from "./Component/pokeCard";
-import Pokemon from "./pokemon.type";
-import { Grid } from "@mui/material";
+import './styles.css';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import PokeCard from './Component/pokeCard';
+import Pokemon from './pokemon.type';
+import { Grid } from '@mui/material';
 
 const App = () => {
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [pokeApiUrl, setPokeApiUrl] = useState(
-    "https://pokeapi.co/api/v2/pokemon/"
-  );
+  const [pokeApiUrl, setPokeApiUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
 
   function fetchPokemon() {
     fetch(pokeApiUrl)
@@ -23,19 +21,10 @@ const App = () => {
   return (
     <>
       <div className="App d-flex flex-column align-items-center">
-        <img
-          src="https://i.pinimg.com/originals/9e/39/23/9e3923825ba4a4fa967858f980b8460f.png"
-          alt="Pokemon"
-          className="image-container"
-        />
+        <img src="https://i.pinimg.com/originals/9e/39/23/9e3923825ba4a4fa967858f980b8460f.png" alt="Pokemon" className="image-container" />
         <Grid container spacing={2}>
           {pokemonList.map((pokemon, index) => (
-            <PokeCard
-              key={index}
-              name={pokemon.name}
-              url={pokemon.url}
-              id={index + 1}
-            />
+            <PokeCard key={index} name={pokemon.name} url={pokemon.url} id={index + 1} />
           ))}
         </Grid>
         <Button className="m-2" variant="contained" onClick={fetchPokemon}>
